febd81b511272dfa601f62d94386e3b588e53818
Magali L. Espinosa <magalizethe@gmail.com>
2025-05-26 23:21:42 -0600

    Se terminó el ejercicio de la cadena

---
+++ cadena.js
@@ -0,0 +1,19 @@
+let s = "Saludo ";
+let c = "coordial ";
+
+console.log(s +"fuerte y "+ c + "para todos ");
+
+//crear una nueva cadena
+
+let nueva = s +"fuerte y "+ c + "para todos ";
+
+console.log(nueva);
+
+let num1 = 1;
+let num2 ="1";
+let num3 = 2;
+console.log(num1+num2); // Suma de número más cadena= cadena
+
+console.log(num1 + 1); //Suma de número más número = número
+
+console.log(num1+num3);
